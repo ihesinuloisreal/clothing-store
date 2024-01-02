888cc66fbfe82741e5685ef34f30d93eaf55bc09
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2024-01-02 14:53:16 -0800

    Started Working on Button Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from './pages/homePage/HomePage'
 import ShopPage from './pages/shopPage/ShopPage';
 import Header from './components/header/header.component';
+import SignInAndSingUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
@@ -11,10 +12,11 @@ function App() {
   return (
     <div>
     <Header/>
-    <Routes>
+    <Routes> 
 
       <Route exact path="/" element={<HomePage/>}/>
       <Route path="/shop" element={<ShopPage/>}/>
+      <Route path="/signin" element={<SignInAndSingUpPage/>}/>
 
     </Routes>
       
