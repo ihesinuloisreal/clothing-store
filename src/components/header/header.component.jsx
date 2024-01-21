8d72868a76af48a60801d69d14e220238ad4b30a
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2024-01-20 17:03:01 -0800

    Added redux command for toggling the cart button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.styles.scss'
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils';
 import {connect} from 'react-redux'
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 
-const Header = ({currentUser}) =>{
+
+const Header = ({currentUser, hidden}) =>{
     return (
         <div className="header">
         
@@ -27,14 +30,19 @@ const Header = ({currentUser}) =>{
                         : 
                         (<Link className="option" to='/signin'> SIGN IN</Link>)
                 }
-                
+                <CartIcon />
             </div>
+            {
+            hidden ? null : <CartDropdown/>
+
+            }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}})=> ({
+    currentUser,
+    hidden
 })
 
 
